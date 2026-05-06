e7b520d4d6e3963978c0fd377cb14c00d9140714
Raju <mhraju069@gmail.com>
2026-05-06 16:18:17 +0600

    fix : update cart items

--- product/views.py
+++ product/views.py
@@ -1,6 +1,9 @@
-from rest_framework import generics
+from rest_framework import generics,status
 from .serializers import *
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import *
+
 
 
 class GetFoodItemApiView(generics.ListAPIView):
@@ -17,6 +20,92 @@ class GetFoodItemApiView(generics.ListAPIView):
         serializer = self.get_serializer(queryset, many=True)
         return Response({
             "success": True,
-            "message": "Data fetched successfully",
+            "message": "Food items fetched successfully",
             "data": serializer.data
         })
+
+
+
+
+class GetcartItemsApiView(generics.ListAPIView):
+    serializer_class = ProductCartSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        queryset, _ = ProductCart.objects.get_or_create(user=self.request.user)
+        return queryset
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset)
+        
+        return Response({
+            "success": True,
+            "message": "Cart items fetched successfully",
+            "data": serializer.data
+        })
+
+
+
+
+class UpdateCartApiView(generics.GenericAPIView):
+    serializer_class = CartUpdateSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request,action):
+        serializer = self.get_serializer(data=request.data)
+
+        if action not in ['add', 'remove']:
+            return Response({
+                "success": False,
+                "message": "Invalid action",
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        if not serializer.is_valid():
+            return Response({
+                "success": False,
+                "message": "Invalid data",
+                "errors": serializer.errors
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        food_item_id = serializer.validated_data['food_item_id']
+        quantity = serializer.validated_data['quantity']
+
+        try:
+            food_item = FoodItem.objects.filter(id=food_item_id).first()
+
+            if not food_item:
+                return Response({
+                    "success": False,
+                    "message": "Food item not found",
+                }, status=status.HTTP_404_NOT_FOUND)
+                
+            cart,_ = ProductCart.objects.get_or_create(user=self.request.user)
+            item, created = CartItems.objects.get_or_create(cart=cart,food_item=food_item,defaults={'quantity':quantity})
+
+            if action == 'add':
+                if not created:
+                    item.quantity+= quantity
+
+            elif action == 'remove':
+                item.quantity-= quantity
+                if item.quantity <= 0:
+                    item.delete()
+
+                    return Response({
+                        "success": True,
+                        "message": "Cart item deleted successfully",
+                    })
+            
+            item.save()
+                    
+            return Response({
+                "success": True,
+                "message": "Cart items added successfully",
+            })
+
+        except Exception as e:
+            return Response({
+                "success": False,
+                "message": str(e),
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
