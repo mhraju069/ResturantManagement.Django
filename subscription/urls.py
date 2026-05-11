b4e41fe444c1d3e4aba5ef4636839c8940c288db
Raju <mhraju069@gmail.com>
2026-05-11 09:41:13 +0600

    update: oeder places api

--- subscription/urls.py
+++ subscription/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('plans/', GetPlans.as_view(), name='plans'),
-    path('myplan/', GetMySubscription.as_view(), name='myplan'),
+    #path('plans/', GetPlans.as_view(), name='plans'),
+    #path('myplan/', GetMySubscription.as_view(), name='myplan'),
 ]
 
