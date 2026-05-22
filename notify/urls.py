83dfc82adb5eeafa7ddf712e0b4132baebeb6eed
Raju <mhraju069@gmail.com>
2026-05-22 14:56:18 +0600

    fix: notification system

--- notify/urls.py
+++ notify/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterDeviceView, NotificationListView, MarkNotificationReadView, DashboardDataAPIView
+from .views import RegisterDeviceView, NotificationListView, MarkNotificationReadView, DashboardDataAPIView, FirebaseConfigAPIView
 
 urlpatterns = [
     path('register-device/', RegisterDeviceView.as_view(), name='register_device'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('mark-read/', MarkNotificationReadView.as_view(), name='mark_all_read'),
     path('mark-read/<int:pk>/', MarkNotificationReadView.as_view(), name='mark_single_read'),
     path('dashboard-api/', DashboardDataAPIView.as_view(), name='dashboard_data_api'),
+    path('firebase-config/', FirebaseConfigAPIView.as_view(), name='firebase_config'),
 ]
