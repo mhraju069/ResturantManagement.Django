3409fec7d455cbd47d80e4d4a1422f4c95d380a7
Raju <mhraju069@gmail.com>
2026-07-04 12:39:38 +0600

    Update: settings

--- config/settings.py
+++ config/settings.py
@@ -185,7 +185,7 @@ SIMPLE_JWT = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_PORT = int(os.getenv("EMAIL_PORT") or 587)
 EMAIL_TIMEOUT = 15
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
